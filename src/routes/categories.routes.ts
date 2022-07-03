40dc0af7d3afa0652d249093cdfacd85aa624fc6
RenanRSilva <renanrambuls@gmail.com>
2022-07-03 15:33:04 -0300

    initial commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const category: Category = {
+    name,
+    description,
+    id: uuidv4(),
+    created_at: new Date(),
+  };
+
+  categories.push(category);
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };

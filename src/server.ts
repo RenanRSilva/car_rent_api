9ce8289b13a22d45d3bc6b19e5f009a3d6d79628
RenanRSilva <renanrambuls@gmail.com>
2022-07-11 17:03:00 -0300

    chore: routes refactor

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import express from "express";
-
-import { specificationRoutes } from "./routes/specifications.routes";
-
-
-import { categoriesRoutes } from "./routes/categories.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
-
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationRoutes);
+app.use(router);
 
 app.listen(4444, () => console.log("Server is running!"));

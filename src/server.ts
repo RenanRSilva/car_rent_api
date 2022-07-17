808f6f5b7ddc0b1b2f150ab997c1ca8f21c3f20a
RenanRSilva <renanrambuls@gmail.com>
2022-07-17 15:01:50 -0300

    docs with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express"
+
 import { router } from "./routes";
+import swaggerFile from './swagger.json'
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use("/api-docs", swaggerUi.server, swaggerUi.setup(swaggerFile))
+
 
 app.listen(4444, () => console.log("Server is running!"));
